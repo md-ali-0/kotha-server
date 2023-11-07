d071096bdf4a68302a55c4267876f9073f147d71
md-ali-0 <mohammad.98482@gmail.com>
2023-11-07 22:46:25 +0600

    Category Details get by single category id

--- index.js
+++ index.js
@@ -2,43 +2,57 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { config } from 'dotenv';
 import express from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 config();
-app.use(cors(
-    {
+app.use(
+    cors({
         origin: ['http://localhost:5173'],
-        credentials: true
-    }
-));
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.daanzm4.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri,{
+const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-      }
-})
-const userCollections = client.db('kothaDB').collection('userCollections')
-const categoryCollections = client.db('kothaDB').collection('categoryCollections')
+    },
+});
+const userCollections = client.db('kothaDB').collection('userCollections');
+const categoryCollections = client
+    .db('kothaDB')
+    .collection('categoryCollections');
 
 app.get('/', (req, res) => {
     res.send('Kotha Server is running');
 });
-
-app.post('/add-category', async(req,res)=>{
-    const category = req.body
-    const result = await categoryCollections.insertOne(category)
+app.get('/categories', async (req, res) => {
+    const result = await categoryCollections.find().toArray();
+    const totalCategories = result.length
+    res.send({result, totalCategories})
+});
+app.get('/category/:id', async (req, res) => {
+    const id = req.params.id
+    const filter = {
+        _id: new ObjectId(id)
+    }
+    const result = await categoryCollections.findOne(filter)
     res.send(result)
-})
-app.post('/add-user', async (req,res)=>{
+});
+app.post('/add-category', async (req, res) => {
+    const category = req.body;
+    const result = await categoryCollections.insertOne(category);
+    res.send(result);
+});
+app.post('/add-user', async (req, res) => {
     const user = req.body;
 
     // const isUserExits = await userCollections.findOne({email: user.email})
@@ -46,15 +60,15 @@ app.post('/add-user', async (req,res)=>{
     //     return res.status(400).send('User Already Exits')
     // }
 
-    const result = await userCollections.insertOne(user)
-    res.send(result)
-})
-app.put('/edit-user', async (req,res)=>{
+    const result = await userCollections.insertOne(user);
+    res.send(result);
+});
+app.put('/edit-user', async (req, res) => {
     const user = req.body;
 
     const filter = {
         email: user.email,
-    }
+    };
     const values = {
         $set: {
             name: user.name,
@@ -63,13 +77,13 @@ app.put('/edit-user', async (req,res)=>{
             createdAt: user.createdAt,
             lastSignInTime: user.lastSignInTime,
         },
-    }
+    };
     const option = {
-        upsert: true
-    }
-    const result = await userCollections.updateOne(filter, values, option)
-    res.send(result)
-})
+        upsert: true,
+    };
+    const result = await userCollections.updateOne(filter, values, option);
+    res.send(result);
+});
 
 app.listen(port, () => {
     console.log(`Listing .... ${port}`);
